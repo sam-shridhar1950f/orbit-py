8b726551b692e2cab43c4ee7b914066faec8e56c
Jatong Su <jatong@Jatongs-MacBook-Pro.local>
2021-07-15 15:16:20 -0400

    fucking hell

--- tests/threading_test.py
+++ tests/threading_test.py
@@ -14,8 +14,20 @@ magnet_motor = RpiMotorLib.A4988Nema(direction_m, step_m, (21, 21, 21), "DRV8825
 def turn_motor(motor, direction, type, steps, stepdelay,boolean,initdelay):
     motor.motor_go(direction, type, steps, stepdelay, boolean, initdelay)
 
-t1 = threading.Thread(target=turn_motor, args=(rotate_motor, CLOCKWISE, 'Full', 800, 0.0243157, False, 0.05))
-t2 = threading.Thread(target=turn_motor, args=(magnet_motor, CLOCKWISE, 'Full', 2400, 0.003, False, 0.05))
+def turn_rotate_motor():
+    rotate_motor.motor_go(CLOCKWISE, 'Full', 800, 0.005, False, 0.001)
+def turn_magnet_motor():
+    magnet_motor.motor_go(not CLOCKWISE, 'Full', 100, 0.006, False, 0.001)
+    magnet_motor.motor_go(CLOCKWISE, 'Full', 100, 0.003, False, 0.001)
+    magnet_motor.motor_go(not CLOCKWISE, 'Full', 100, 0.006, False, 0.001)
+    magnet_motor.motor_go(CLOCKWISE, 'Full', 100, 0.003, False, 0.001)
+    magnet_motor.motor_go(not CLOCKWISE, 'Full', 100, 0.006, False, 0.001)
+    magnet_motor.motor_go(CLOCKWISE, 'Full', 100, 0.003, False, 0.001)
+    magnet_motor.motor_go(not CLOCKWISE, 'Full', 100, 0.006, False, 0.001)
+    magnet_motor.motor_go(CLOCKWISE, 'Full', 100, 0.003, False, 0.001)
+
+t1 = threading.Thread(target=turn_rotate_motor)
+t2 = threading.Thread(target=turn_magnet_motor)
 
 t1.start()
 t2.start()
